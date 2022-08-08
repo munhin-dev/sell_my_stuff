90dd8891b8a759d71849cd185c7fa0ad82d33fe8
Mun Hin <obh555@gmail.com>
2022-08-08 19:23:09 +0800

    adjust config

--- config/index.js
+++ config/index.js
@@ -11,15 +11,21 @@ const transporter = nodemailer.createTransport({
 
 const mailContent = (order_number, name) => `\nConfirmation Number: ${order_number}\nHello ${name},\n\nWe're happy to let you know that we've received your order.\n\nOnce your package ships, we will send you an email with a tracking number and link so you can see the movement of your package.\n\nIf you have any questions, contact us here by replying to this email directly!\n\nWe are here to help!\n\nThe SellMyStuff Team`;
 
-const sessionConfig = {
-  secret: "keyboardcat",
-  resave: false,
-  saveUninitialized: false,
-  cookie: { 
-    maxAge: 60 * 60 * 24 * 1000,
-    secure: true, 
-    sameSite: "none" 
-  },
+const sessionConfig = () => {
+  if (process.env.NODE_ENV === "production") {
+    return {
+      secret: "keyboardcat",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60 * 60 * 24 * 1000,
+        secure: true,
+        sameSite: "none",
+      },
+    };
+  } else {
+    return { secret: "keyboardcat", resave: false, saveUninitialized: true, cookie: { secure: false } };
+  }
 };
 
 const stripeKey = "sk_test_51LNPqSEzYab6HLIKoQ36q0fKkNLrGQLoNkUOepySj0xIeEyuH2ELacSZ1e1IFvxhRXhnjuMPFcnowxWEuBzFcRcz00ln3zeJhj";
